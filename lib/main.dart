b6fdab1aeb54f6925840ed9216c5fddc91991ec5
Aniket Nikam <91717365+NikamAniket@users.noreply.github.com>
2021-10-28 12:05:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tic Tac Toe',
+      title: 'O X Y',
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
